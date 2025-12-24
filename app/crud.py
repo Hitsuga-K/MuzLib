c93215d75e4fc678bc9842c1fe1c71590c33f8a2
Hitsuga-K <kler228.kapinuz@gmail.com>
2025-12-25 01:35:14 +0300

    Ver2

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import Session
-from app import models
+def get_products(db: Session):
+    return db.query(models.Product).all()
 
-def get_songs(db: Session):
-    return db.query(models.Song).all()
+def create_product(db: Session, product: models.Product):
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return product
+
+def get_reviews(db: Session, product_id: int):
+    return db.query(models.Review).filter(models.Review.product_id == product_id).all()
+
+def create_review(db: Session, review: models.Review):
+    db.add(review)
+    db.commit()
+    db.refresh(review)
+    return review
--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,29 @@
 from pydantic import BaseModel
 
-class UserCreate(BaseModel):
+class ProductBase(BaseModel):
     name: str
+    description: str | None = None
+    price: float
+    image_url: str | None = None
 
-class UserRead(BaseModel):
+class ProductCreate(ProductBase):
+    pass
+
+class ProductRead(ProductBase):
     id: int
-    name: str
-
     class Config:
-        from_attributes = True  # заменяет orm_mode=True в Pydantic v2
+        from_attributes = True
+
+class ReviewBase(BaseModel):
+    user_name: str
+    rating: int
+    comment: str | None = None
+
+class ReviewCreate(ReviewBase):
+    product_id: int
+
+class ReviewRead(ReviewBase):
+    id: int
+    product_id: int
+    class Config:
+        from_attributes = True
